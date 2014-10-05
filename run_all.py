2e5a78ffd2e04c16a81c5556ca191b26f042ab86
seanp <devnull@localhost>
2014-10-05 00:39:41 +0000

    fixed bug in run script

--- run_all.py
+++ run_all.py
@@ -1,30 +1,36 @@
-from subprocess import check_output
+#from subprocess import check_output
 from numpy import mean, std
+import time
+import subprocess
 locks = ["clh","spinlock","mcs","array","ticket","ttas","ttas_simp","rw","rw_fair","rw_scalable_fair"]
-# locks = ["ttas","rw","rw_fair"]
+#locks = ["clh"]
 # (locks,lock holding time, pause time between aquisitionss)
 configs =[(1,0,0),(1,300,300),(1,300,1250),(1,300,5000)]
+#configs =[(1,300,1250)]
 #configs =[(1,100,1000),(1,0,0),(1,10,1000),(1,250,1000),(1,1000,1000),(1,10000,10000)]
 num_samples = 10 
 
 for (l,h,p) in configs: 
     print ("locks: " + str(l) + " acquire time: " + str(h) + " pause time: " +
 		str(p))
-    fname = "tdelayarm9"+str(l)+"a"+str(h)+"p"+str(p)+".dat"
+    fname = "arm9a"+str(h)+"p"+str(p)+".dat"
     file = open(fname,'w')
     legend = ["threads"]
     for t in [1,2,3,4]:
+#    for t in [3,4]:
 	results =[t]
         for lock in locks:
 	    samples = []
 	    i = 0
-
-	    dummy_run = check_output(["libslock/stress_test_" + lock + "s", "-p " + str(p)
-			, "-a " + str(h), "-l " + str(l), "-n " + str(t), "-d 1000"])
+	    command = ["libslock/stress_test_" + lock + "s", "-p", str(p),  
+		"-a" , str(h), "-l" , str(l), "-n" , str(t), "-d", "10000", "-c", "4"] 
+	    print command
+	    dummy_run = subprocess.Popen(command, stdout=subprocess.PIPE)
+            tout, terr = dummy_run.communicate()
 	    while i < num_samples:
                 i = i + 1
-		output = check_output(["libslock/stress_test_" + lock + "s", "-p " + 
-			str(p), "-a " + str(h), "-l " + str(l), "-n " + str(t), "-d 10000", "-c 4"])
+		proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+		output, err = proc.communicate()
 		print (lock + " tmp: " + output)
 		samples = samples+[float(output)]
 	    smean = mean(samples)
